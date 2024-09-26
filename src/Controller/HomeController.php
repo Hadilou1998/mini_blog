d5cbb8e30f39839fcd9f294e60d4e9749b024090
Hadilou1998 <hadilissilame@gmail.com>
2024-09-26 16:06:41 +0200

    Updated HomeController.php

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,19 +6,22 @@ use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(ArticleRepository $articles): Response
+    public function index(ArticleRepository $articles, UrlGeneratorInterface $urlGenerator): Response
     {
+        $articleIndexRoute = $this->generateUrl('article_index');
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
             'articles' => $articles->findBy(
                 [],
                 ['created_at' => 'DESC'],
                 6
-            )
+            ),
+            'articleIndexRoute' => $articleIndexRoute
         ]);
     }
 }
